c15b5e1d0b06599842f04c68a0fac0fbc2a6b4ca
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2023-11-30 18:50:53 +0900

    feat: implement Tabular correctly

--- src/commonMain/kotlin/Tabular.kt
+++ src/commonMain/kotlin/Tabular.kt
@@ -1,7 +1,12 @@
 package com.github.shwaka.kotex
 
+import kotlin.math.max
+
 public interface TabularInterface<T : TexScriptInterface<T>> : TexScriptInterface<T> {
+    public var colCount: Int
+
     public fun addRow(cells: List<String>) {
+        this.colCount = max(this.colCount, cells.size)
         val line: String = cells.joinToString(" & ") + " \\\\"
         this.addLines(line)
     }
@@ -14,13 +19,28 @@ public interface TabularInterface<T : TexScriptInterface<T>> : TexScriptInterfac
 public class Tabular(linePrefix: String = "") :
     ScriptBase<Tabular>(linePrefix),
     TabularInterface<Tabular> {
+
+    override var colCount: Int = 0
+
+    override fun newScript(linePrefix: String): Tabular {
+        return Tabular(linePrefix)
+    }
+
+    override fun toStringList(): List<String> {
+        val texDocument = TexDocument {
+            begin(
+                "tabular",
+                argument = "c".repeat(this@Tabular.colCount),
+            ) {
+                addLines(super.toStringList())
+            }
+        }
+        return texDocument.toStringList()
+    }
+
     public companion object {
         public operator fun invoke(linePrefix: String = "", block: Tabular.() -> Unit): Tabular {
             return Tabular(linePrefix).apply(block)
         }
-    }
-
-    override fun newScript(linePrefix: String): Tabular {
-        return Tabular(linePrefix)
     }
 }
